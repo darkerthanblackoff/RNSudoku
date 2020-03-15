180fcc4e460043cc0fedc51af16461dab764e697
Mihail Lemeza <darkerthanblackoff@gmail.com>
2020-03-15 21:58:27 +0300

    Added persist

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,18 @@ import { StatusBar } from 'react-native';
 import { Router } from './router';
 
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
+
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 const App = () => {
   return (
     <>
       <StatusBar backgroundColor="#6A4D6B" />
       <Provider store={store}>
-        <Router />
+        <PersistGate persistor={persistor} loading={null}>
+          <Router />
+        </PersistGate>
       </Provider>
     </>
   );
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -2,9 +2,22 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
 
+import { REDUCERS } from '../../constants';
+import AsyncStorage from '@react-native-community/async-storage';
+import { PersistConfig, persistReducer, persistStore } from 'redux-persist';
+
+const persistConfig: PersistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: [REDUCERS.GAME, REDUCERS.LEADER_BOARD, REDUCERS.SETTINGS],
+};
+
+const persistedReducers = persistReducer(persistConfig, reducers);
+
 import logger from 'redux-logger';
 
 const middleware = applyMiddleware(thunk, logger);
-const store = createStore(reducers, middleware);
+const store = createStore(persistedReducers, middleware);
+const persistor = persistStore(store);
 
-export default store;
+export { persistor, store };
